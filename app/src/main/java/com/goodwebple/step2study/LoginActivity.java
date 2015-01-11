9451614956b31f7d0d20f5eb3ca5cad4dbd83f3f
jooneyp <parkjydev@gmail.com>
2015-01-12 03:13:00 +0900

    login page editing
    only view.

---
+++ app/src/main/java/com/goodwebple/step2study/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.goodwebple.step2study;
+
+import android.app.Activity;
+
+/**
+ * Created by j8n3y on 15. 1. 6..
+ */
+public class LoginActivity extends Activity {
+
+}
--- app/src/main/java/com/goodwebple/step2study/RankModeActivity.java
+++ app/src/main/java/com/goodwebple/step2study/RankModeActivity.java
@@ -9,78 +9,111 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by j8n3y on 15. 1. 3..
  */
 public class RankModeActivity extends Activity {
+    private static ArrayList<Activity> activities = new ArrayList<Activity>();
+
+    final int ANS_WRONG = 0;
+    final int ANS_CORRECT = 1;
+    final int INIT_COLOR = 2;
+
     SQLiteDatabase db;
     DatabaseHelper helper;
 
-    private final String QUESTION_NUM = "100";
+    private final String QUESTION_NUM = "5";
+
+    String QUESTION = "mean";
+    String ANSWER = "word";
 
     private int score = 0;
     private int ansNum;
-    private int qNum = 1;
     Cursor wordData = null;
 
     private String cardpack;
 
-    private TextView question;
-    private TextView ans1;
-    private TextView ans2;
-    private TextView ans3;
-    private TextView ans4;
+    private TextView tView_question;
+    private TextView tView_ans1;
+    private TextView tView_ans2;
+    private TextView tView_ans3;
+    private TextView tView_ans4;
+    private FrameLayout frame_inner;
+    private TextView text_score;
+    private Button btn_retry;
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        stopTimer();
+        if(timer != null) {
+            Thread tmpThread = timer;
+            timer = null;
+            tmpThread.interrupt();
+        }
+        activities.remove(this);
+        Log.i("Destroy", "!!!");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mode_rank);
+        activities.add(this);
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        question = (TextView) findViewById(R.id.text_question);
-        ans1 = (TextView) findViewById(R.id.text_answer1);
-        ans2 = (TextView) findViewById(R.id.text_answer2);
-        ans3 = (TextView) findViewById(R.id.text_answer3);
-        ans4 = (TextView) findViewById(R.id.text_answer4);
+        timeBar = (ProgressBar) findViewById(R.id.progressBar);
+        tView_question = (TextView) findViewById(R.id.text_question);
+        tView_ans1 = (TextView) findViewById(R.id.text_answer1);
+        tView_ans2 = (TextView) findViewById(R.id.text_answer2);
+        tView_ans3 = (TextView) findViewById(R.id.text_answer3);
+        tView_ans4 = (TextView) findViewById(R.id.text_answer4);
+
+        frame_inner = (FrameLayout) findViewById(R.id.frame_inner);
+        text_score = (TextView) findViewById(R.id.text_score);
+        btn_retry = (Button) findViewById(R.id.btn_retry);
+
+        frame_inner.setVisibility(View.INVISIBLE);
 
         // TODO: should receive name of Cardpack from parent Activity
         cardpack = "wordlist";
 
         try {
-            makeQuestion();
+            readQuestion();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        btn_retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frame_inner.setVisibility(View.INVISIBLE);
+                try {
+                    readQuestion();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                changeBGColor(INIT_COLOR);
+                printQuestion();
+            }
+        });
 
         selectedAnswerListener sAL = new selectedAnswerListener();
 
-        ans1.setOnClickListener(sAL);
-        ans1.setTag(1);
-        ans2.setOnClickListener(sAL);
-        ans2.setTag(2);
-        ans3.setOnClickListener(sAL);
-        ans3.setTag(3);
-        ans4.setOnClickListener(sAL);
-        ans4.setTag(4);
+        tView_ans1.setOnClickListener(sAL); tView_ans1.setTag(1);
+        tView_ans2.setOnClickListener(sAL); tView_ans2.setTag(2);
+        tView_ans3.setOnClickListener(sAL); tView_ans3.setTag(3);
+        tView_ans4.setOnClickListener(sAL); tView_ans4.setTag(4);
 
-        Log.i("0", "onCreate");
-        printQuestion1();
+        printQuestion();
     }
-
-
 
     public class selectedAnswerListener implements View.OnClickListener {
 
@@ -89,18 +122,17 @@ public class RankModeActivity extends Activity {
             if (ans == ansNum) {
                 score += currentTime;
                 Toast.makeText(getApplicationContext(), "Correct. Score : " + score, Toast.LENGTH_SHORT).show();
-                changeBGColor(1);
+                changeBGColor(ANS_CORRECT);
             } else {
                 Toast.makeText(getApplicationContext(), "Wrong. Score : " + score, Toast.LENGTH_SHORT).show();
-                changeBGColor(0);
+                changeBGColor(ANS_WRONG);
             }
             stopTimer();
-            Log.i("stopTimer", "Time stopped");
         }
     }
 
-    public void makeQuestion() throws SQLException {
-        String[] columns = new String[]{"mean", "word"};
+    public void readQuestion() throws SQLException {
+        String[] columns = new String[]{ QUESTION, ANSWER };
 
         helper = new DatabaseHelper(getApplicationContext());
         db = helper.getReadableDatabase();
@@ -108,73 +140,55 @@ public class RankModeActivity extends Activity {
         wordData.moveToFirst();
     }
 
-    public void printQuestion1() {
-        qNum ++;
-        String mean = wordData.getString(0);
-        String word = wordData.getString(1);
-        startTimer();
-        printQuestion2(mean, word);
-    }
+    public void printQuestion() {
+        String question = wordData.getString(0);
+        String answer = wordData.getString(1);
 
-    public void printQuestion2(String mean, String word) {
-        Log.i("6", "printQuestion2");
         Random iRan = new Random();
         ansNum = iRan.nextInt(4) + 1;
 
         Cursor ranAnsData = null;
-        String[] columns = new String[]{"word"};
+        String[] columns = new String[]{ ANSWER };
 
         helper = new DatabaseHelper(getApplicationContext());
         db = helper.getReadableDatabase();
         ranAnsData = db.query(cardpack, columns, null, null, null, null, "RANDOM()", "3");
-        Log.i("ranAnsData", ranAnsData.toString());
-        Log.i("ranAnsDataCount", String.valueOf(ranAnsData.getCount()));
-        Log.i("ans", String.valueOf(ansNum));
 
         if (ranAnsData != null && ranAnsData.moveToFirst()) {
-            Log.i("ranAnsData", ranAnsData.getString(0));
-            question.setText(mean);
+            startTimer();
+            this.tView_question.setText(question);
             switch (ansNum) {
-                case 0:
                 case 1:
                     Log.i("!!ANSWER!!", "1");
                     ranAnsData.moveToFirst();
-                    ans1.setText(word);
-                    ans2.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans3.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans4.setText(ranAnsData.getString(0));
+                    tView_ans1.setText(answer);
+                    tView_ans2.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans3.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans4.setText(ranAnsData.getString(0));
                     break;
                 case 2:
                     Log.i("!!ANSWER!!", "2");
                     ranAnsData.moveToFirst();
-                    ans1.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans2.setText(word);
-                    ans3.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans4.setText(ranAnsData.getString(0));
+                    tView_ans1.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans2.setText(answer);
+                    tView_ans3.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans4.setText(ranAnsData.getString(0));
                     break;
                 case 3:
                     Log.i("!!ANSWER!!", "3");
                     ranAnsData.moveToFirst();
-                    ans1.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans2.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans3.setText(word);
-                    ans4.setText(ranAnsData.getString(0));
+                    tView_ans1.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans2.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans3.setText(answer);
+                    tView_ans4.setText(ranAnsData.getString(0));
                     break;
                 case 4:
                     Log.i("!!ANSWER!!", "4");
                     ranAnsData.moveToFirst();
-                    ans1.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans2.setText(ranAnsData.getString(0));
-                    ranAnsData.moveToNext();
-                    ans3.setText(ranAnsData.getString(0));
-                    ans4.setText(word);
+                    tView_ans1.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans2.setText(ranAnsData.getString(0)); ranAnsData.moveToNext();
+                    tView_ans3.setText(ranAnsData.getString(0));
+                    tView_ans4.setText(answer);
                     break;
             }
         }
@@ -186,28 +200,20 @@ public class RankModeActivity extends Activity {
             switch (ansNum) {
                 case 0:
                 case 1:
-                    ans1.setBackgroundColor(Color.GREEN);
-                    ans2.setBackgroundColor(Color.BLUE);
-                    ans3.setBackgroundColor(Color.BLUE);
-                    ans4.setBackgroundColor(Color.BLUE);
+                    tView_ans1.setBackgroundColor(Color.GREEN); tView_ans2.setBackgroundColor(Color.BLUE);
+                    tView_ans3.setBackgroundColor(Color.BLUE); tView_ans4.setBackgroundColor(Color.BLUE);
                     break;
                 case 2:
-                    ans1.setBackgroundColor(Color.BLUE);
-                    ans2.setBackgroundColor(Color.GREEN);
-                    ans3.setBackgroundColor(Color.BLUE);
-                    ans4.setBackgroundColor(Color.BLUE);
+                    tView_ans1.setBackgroundColor(Color.BLUE); tView_ans2.setBackgroundColor(Color.GREEN);
+                    tView_ans3.setBackgroundColor(Color.BLUE); tView_ans4.setBackgroundColor(Color.BLUE);
                     break;
                 case 3:
-                    ans1.setBackgroundColor(Color.BLUE);
-                    ans2.setBackgroundColor(Color.BLUE);
-                    ans3.setBackgroundColor(Color.GREEN);
-                    ans4.setBackgroundColor(Color.BLUE);
+                    tView_ans1.setBackgroundColor(Color.BLUE); tView_ans2.setBackgroundColor(Color.BLUE);
+                    tView_ans3.setBackgroundColor(Color.GREEN); tView_ans4.setBackgroundColor(Color.BLUE);
                     break;
                 case 4:
-                    ans1.setBackgroundColor(Color.BLUE);
-                    ans2.setBackgroundColor(Color.BLUE);
-                    ans3.setBackgroundColor(Color.BLUE);
-                    ans4.setBackgroundColor(Color.GREEN);
+                    tView_ans1.setBackgroundColor(Color.BLUE); tView_ans2.setBackgroundColor(Color.BLUE);
+                    tView_ans3.setBackgroundColor(Color.BLUE); tView_ans4.setBackgroundColor(Color.GREEN);
                     break;
                 default:
                     Toast.makeText(getApplicationContext(), "ERROR!", Toast.LENGTH_SHORT);
@@ -217,54 +223,40 @@ public class RankModeActivity extends Activity {
             switch (ansNum) {
                 case 0:
                 case 1:
-                    ans1.setBackgroundColor(Color.BLUE);
-                    ans2.setBackgroundColor(Color.RED);
-                    ans3.setBackgroundColor(Color.RED);
-                    ans4.setBackgroundColor(Color.RED);
+                    tView_ans1.setBackgroundColor(Color.BLUE); tView_ans2.setBackgroundColor(Color.RED);
+                    tView_ans3.setBackgroundColor(Color.RED); tView_ans4.setBackgroundColor(Color.RED);
                     break;
                 case 2:
-                    ans1.setBackgroundColor(Color.RED);
-                    ans2.setBackgroundColor(Color.BLUE);
-                    ans3.setBackgroundColor(Color.RED);
-                    ans4.setBackgroundColor(Color.RED);
+                    tView_ans1.setBackgroundColor(Color.RED); tView_ans2.setBackgroundColor(Color.BLUE);
+                    tView_ans3.setBackgroundColor(Color.RED); tView_ans4.setBackgroundColor(Color.RED);
                     break;
                 case 3:
-                    ans1.setBackgroundColor(Color.RED);
-                    ans2.setBackgroundColor(Color.RED);
-                    ans3.setBackgroundColor(Color.BLUE);
-                    ans4.setBackgroundColor(Color.RED);
+                    tView_ans1.setBackgroundColor(Color.RED); tView_ans2.setBackgroundColor(Color.RED);
+                    tView_ans3.setBackgroundColor(Color.BLUE); tView_ans4.setBackgroundColor(Color.RED);
                     break;
                 case 4:
-                    ans1.setBackgroundColor(Color.RED);
-                    ans2.setBackgroundColor(Color.RED);
-                    ans3.setBackgroundColor(Color.RED);
-                    ans4.setBackgroundColor(Color.BLUE);
+                    tView_ans1.setBackgroundColor(Color.RED); tView_ans2.setBackgroundColor(Color.RED);
+                    tView_ans3.setBackgroundColor(Color.RED); tView_ans4.setBackgroundColor(Color.BLUE);
                     break;
                 default:
                     Toast.makeText(getApplicationContext(), "ERROR!", Toast.LENGTH_SHORT);
             }
         } else if (correct == 2) {
-            ans1.setBackgroundColor(Color.WHITE);
-            ans2.setBackgroundColor(Color.WHITE);
-            ans3.setBackgroundColor(Color.WHITE);
-            ans4.setBackgroundColor(Color.WHITE);
+            tView_ans1.setBackgroundColor(Color.WHITE); tView_ans2.setBackgroundColor(Color.WHITE);
+            tView_ans3.setBackgroundColor(Color.WHITE); tView_ans4.setBackgroundColor(Color.WHITE);
         }
-        Log.i("8", "BGColor changed");
     }
 
     /*
-        TIMER Threads
+        TIMER Methods
     */
-
-
-    private ProgressBar progressBar;
+    private ProgressBar timeBar;
     private volatile Thread timer = null;
     private int currentTime = 100;
 
     public synchronized void startTimer() {
         timer = null;
         timer = new Thread(null, timeChecker, "Timer");
-        Log.i("timer!!@@", "right before timer starts");
         timer.start();
     }
 
@@ -273,18 +265,22 @@ public class RankModeActivity extends Activity {
             Thread tmpThread = timer;
             timer = null;
             tmpThread.interrupt();
-
-            final Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    changeBGColor(2);
-                    wordData.moveToNext();
-                    printQuestion1();
-                }
-            }, 2000);
+            if(wordData.moveToNext()) {
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        changeBGColor(INIT_COLOR);
+                        printQuestion();
+                    }
+                }, 1500);
+            } else {
+                frame_inner.setVisibility(View.VISIBLE);
+                text_score.setText(String.valueOf(score));
+                score = 0;
+            }
         } else {
-            Log.i("stopTimer", "null");
+            Log.i("stopTimer", "Timer Not Stopped");
         }
     }
 
@@ -293,7 +289,7 @@ public class RankModeActivity extends Activity {
         public void run() {
             if(Thread.currentThread() == timer) {
                 currentTime = 100;
-                progressBar.setProgress(currentTime);
+                timeBar.setProgress(currentTime);
                 final int zero = 0;
                 while (currentTime > zero) {
                     try {
@@ -311,15 +307,14 @@ public class RankModeActivity extends Activity {
         Handler progressBarHandle = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                currentTime -= 1;
-                progressBar.setProgress(currentTime);
+                currentTime --;
+                timeBar.setProgress(currentTime);
                 if(currentTime%5 == 0) {
                     Log.i("timer dec!!", String.valueOf(currentTime));
                 }
                 if(currentTime == 0) {
-                    Log.i("Timer STOPPED", "time 0, timerstop");
                     Toast.makeText(getApplicationContext(), "Timeout. Score : " + score, Toast.LENGTH_SHORT).show();
-                    changeBGColor(0);
+                    changeBGColor(ANS_WRONG);
                     stopTimer();
                 }
             }
